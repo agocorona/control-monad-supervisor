5982286e9a50f7a1d8d18c044fea92899d7796f8
Alberto G. Corona <agocorona@gmail.com>
2013-07-18 23:49:34 +0200

    added documentation

--- Control/Monad/Supervisor.hs
+++ Control/Monad/Supervisor.hs
@@ -8,7 +8,10 @@
 -- Stability   :
 -- Portability :
 --
--- |
+-- | A supervisor monad that explore the execution tree of an internal monad and define extra behaviours thanks to flexible instance definitions for each particular purpose.
+-- It can inject new behaviours for backtracking, trace generation, testing, transaction rollbacks etc
+-- The supervisor monad is used in the package MFlow to control the routing, tracing, state management, back button management and navigation in general
+
 --
 -----------------------------------------------------------------------------
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, UndecidableInstances #-}
@@ -16,23 +19,32 @@ module Control.Monad.Supervisor where
 
 import Control.Monad.State
 
+-- | The internal computation can be reexecuted, proceed forward of proceed
 data Control a = Control a | Forward a | Backward
 
+-- | The supervisor add a Control data wrapper that is interpreted by the monad instance
 newtype Sup m a = Sup { runSup :: m (Control a ) }
 
-
+-- | The supervise class add two general controls that can be applied:
 class MonadState s m => Supervise s m where
-   supBack     :: s -> m ()
+   supBack     :: s -> m ()          -- ^ when the computation goes back, what to do with the state
+                                    -- the parameter is the backtraked state that the computation had
+                                    -- before execution. By default, this state will recovered.
    supBack = const $ return ()
    
-   supervise ::    m (Control a) -> m (Control a)
+   supervise ::    m (Control a) -> m (Control a)  -- ^ When the conputation has been executed,
+                                                  -- what to do depending on the result
+                                                  -- this is an opportunity for modifying behaviours
    supervise= id
 
+-- | flag the computation that return the result as a control point
+-- when the computation is going back, it will be reexecuted.
 breturn :: Monad m => a -> Sup m a
 breturn = Sup . return . Control 
 
 instance MonadState s m =>Supervise s m
 
+-- | The Supervisor Monad is in essence an Identity monad when executing Forward.
 instance  Supervise s m => Monad (Sup  m) where
     fail   _ = Sup . return $ Backward
     return x = Sup . return $ Forward x
@@ -40,15 +52,24 @@ instance  Supervise s m => Monad (Sup  m) where
      where
      loop = do
         s <- get
+        -- execution as usual if supervise== id
         v <-  supervise $ runSup x                        
         case v of
-            Forward y  -> supervise $ runSup (f y)         
+            --  a normal execution if supervise== id
+            Forward y  -> supervise $ runSup (f y)
+
+            --   a back was returned by a control, propagate it back
+            Backward  ->  return  Backward
+
+            -- the computaton x was a control point. if the branch of execution goes Backward
+            -- then x will be reexecuted. supBack will control the state backtracking, how much of
+            -- the current state we want to keep and how much we want to backtrack  
             Control y  -> do
                  z <- supervise $ runSup (f y)            
                  case z of
                   Backward  -> supBack s >> loop           
                   other   -> return other
-            Backward  ->  return  Backward
+
 
 instance MonadTrans Sup  where
   lift f = Sup $  f >>= return . Forward
--- Control/Monad/Supervisor/Trace.hs
+++ Control/Monad/Supervisor/Trace.hs
@@ -1,5 +1,52 @@
 {-# LANGUAGE ScopedTypeVariables, UndecidableInstances #-}
 
+-- | add a MonadLoc instance to the Supervisor monad that generate a trace in case of uncaugh exception
+-- . See the MonadLoc package: <http://hackage.haskell.org/package/monadloc>
+--
+-- the package control-monad-loc produces call stacks using monadloc, but the Supervisor monad
+-- produces execution traces thanks to the backtracking mechanism.
+--
+-- The trace is produced after the error is produced not at normal execution time, so it does not generate
+-- overhead in normal execution.
+--
+-- Execute the example at Demos.TraceExample.hs it has an error
+--
+-- > {-# OPTIONS -F -pgmF MonadLoc #-}
+-- > module Demos.TraceExample (
+-- >
+-- > ) where
+-- >
+-- > import Control.Monad.Loc
+-- > import Control.Monad.Supervisor.Trace
+-- > import Control.Monad.Trans
+-- >
+-- > main= runTrace $ do
+-- >    liftIO $ print "hello"
+-- >
+-- >    example
+-- >
+-- > example=
+-- >    if True
+-- >       then  do
+-- >               liftIO $ print "world"
+-- >               liftIO $ undefined
+-- >
+-- >       else liftIO $ print "not there"
+
+-- produce this trace:
+
+-- > "hello"
+-- > "world"
+-- > TraceExample.hs: TRACE (error in the last line):
+-- >
+-- > main, Demos.TraceExample(Demos\TraceExample.hs): (23, 18)
+-- > main, Demos.TraceExample(Demos\TraceExample.hs): (26, 4)
+-- > example, Demos.TraceExample(Demos\TraceExample.hs): (30, 13)
+-- > example, Demos.TraceExample(Demos\TraceExample.hs): (32, 15)
+-- > exception: Prelude.undefined
+
+-- TO DO:  extend it for forward traces and test coverages
+
 module Control.Monad.Supervisor.Trace where
 
 import Control.Monad.Supervisor
